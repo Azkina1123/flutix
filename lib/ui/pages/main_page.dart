aead87e711258a04766b270a19551be29d7da2e7
Azkina1123 <aoshofrina@gmail.com>
2023-10-19 18:52:18 +0800

    feat: add homepage

---
+++ lib/ui/pages/main_page.dart
@@ -0,0 +1,51 @@
+part of "pages.dart";
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int _index = 0;
+
+  final List<Widget> _pages = [
+    const HomePage(),
+    const MyTicketPage(),
+    const ProfilePage()
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages.elementAt(_index),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: lightMode ? Colors.white : colors["cinder"],
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: lightMode ? Colors.black : colors["cerulean-blue"],
+        unselectedItemColor: colors["steel"],
+        currentIndex: _index,
+        onTap: (index) {
+          setState(() {
+            _index = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home"
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home"
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home"
+          ),
+        ],
+      ),
+    );
+  }
+}
